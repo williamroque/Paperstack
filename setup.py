7eaccf600bb2a5a70c1f3490855dbb869887b304
William Roque <william.aroque@gmail.com>
2023-02-28 01:15:04 -0300

    Bumped version.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def readme():
 
 setup(
     name='paperstack',
-    version='1.1.4',
+    version='1.1.5',
     description='A powerful, lightweight, universal bibliography management tool',
     long_description=readme(),
     long_description_content_type='text/markdown',
